6ac2e988cf28b8f664dcc85a6ba38d527b850470
zipang29 <guillaume.claudic@gmail.com>
2015-03-01 15:46:01 +0100

    Correction pour l'activitée des contrôles et ajout d'un bouton dans l'application pour passer d'une vue à l'autre

--- AppliAndroid/GopiwanApp/app/src/main/java/app/gopiwan/gopiwanapp/RechercheWifi.java
+++ AppliAndroid/GopiwanApp/app/src/main/java/app/gopiwan/gopiwanapp/RechercheWifi.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 
 public class RechercheWifi extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class RechercheWifi extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recherche_wifi);
+
+        // Récupère le bouton "boutonControle
+        Button b = (Button) this.findViewById(R.id.boutonControle);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RechercheWifi.this, Controles.class);
+                startActivity(intent);
+            }
+        });
     }
 
 

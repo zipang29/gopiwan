df0a0408b307d8d4820f97207ce52dc8d64af344
zipang29 <guillaume.claudic@gmail.com>
2015-03-09 10:47:21 +0100

    L'appli est maintenant capable de rechercher les réseaux wifi et de se connecter au GiPiWan automatiquement lorsque le réseau est trouvé

--- Dev/Client/Android/AppliAndroid/GopiwanApp/app/src/main/java/app/gopiwan/gopiwanapp/WifiReceiver.java
+++ Dev/Client/Android/AppliAndroid/GopiwanApp/app/src/main/java/app/gopiwan/gopiwanapp/WifiReceiver.java
@@ -1,10 +1,13 @@
 package app.gopiwan.gopiwanapp;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiConfiguration;
+import android.provider.Settings;
 import android.widget.TextView;
 
 import java.util.List;
@@ -33,7 +36,27 @@ public class WifiReceiver extends BroadcastReceiver {
         sb.append("Nombre de connexion trouvées : " + wifiList.size());
 
         for (ScanResult connexion : wifiList) {
-            sb.append(connexion.toString());
+            //sb.append(connexion.toString());
+
+            if (connexion.SSID.equalsIgnoreCase("GoPiWan"))
+            {
+                sb.append("Le robot a été trouvé. Connexion en cours...");
+                WifiConfiguration wc = new WifiConfiguration();
+
+                wc.SSID = "\""+connexion.SSID+"\"";
+                wc.BSSID = connexion.BSSID;
+                wc.status = WifiConfiguration.Status.ENABLED;
+                wc.hiddenSSID = false;
+                wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+
+                int netID = wifi.addNetwork(wc); // add network
+
+                if(wifi.enableNetwork(netID, true)) // enable network, but cannot work???????????
+                {
+                    sb = new StringBuilder();
+                    sb.append("Connecté");
+                }
+            }
         }
 
         TextView listConnexion = (TextView) rechercheWifi.findViewById(R.id.textRechercheWifi);

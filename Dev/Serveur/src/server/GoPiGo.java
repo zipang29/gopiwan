006fae18c51895e5e678e2430f44fb9efb739520
platypus <nicolas.bloyet@gmail.com>
2015-03-30 00:23:51 +0200

    Printemps -> Nettoyage
    Rangement du repertoire Dev/server
    Cela concerne surtout certains vieux trucs qui ne sont plus d'actu, mis
    dans un dossier trash, pour en garder trace.
    Je mets volontairement certains fichiers .class, vu que j'exporte tout
    le contenu du repo sur le pi pour le tester.

--- Dev/Serveur/src/server/GoPiGo.java
+++ Dev/Serveur/src/server/GoPiGo.java
@@ -1,61 +1,47 @@
 package server;
 
-import java.io.IOException;
 
 public class GoPiGo implements GoPiGoMovesI{
+	
+	private GoPiGoCommunication comm ;
 		
 	public GoPiGo(){
 		
+		comm = new GoPiGoCommunication(5005) ;
 	}
 
 	@Override
 	public void turnRight() {
 		if(Globals.verbose) System.out.println("Turn Right");
-		try {
-			Runtime.getRuntime().exec("python turn_right.py") ;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		comm.send("right") ;
 	}
 
 	@Override
 	public void turnLeft() {
 		if(Globals.verbose) System.out.println("Turn Left");
-		try {
-			Runtime.getRuntime().exec("python turn_left.py") ;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		comm.send("left") ;
 	}
 
 	@Override
 	public void forward() {
 		if(Globals.verbose) System.out.println("Go Forward");
-		try {
-			Runtime.getRuntime().exec("python forward.py") ;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		comm.send("forward") ;
 	}
 
 	@Override
 	public void backward() {
 		if(Globals.verbose) System.out.println("Go Backward");
-		try {
-			Runtime.getRuntime().exec("python backward.py") ;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		comm.send("backward") ;
 	}
 
 	@Override
 	public void stop() {
 		
-		try {
-			Runtime.getRuntime().exec("python stop.py") ;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		comm.send("stop") ;
 	}
 
 }

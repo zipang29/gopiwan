12c11e224bc398236fb1e1ff47f55e81278d8957
platypus <nicolas.bloyet@gmail.com>
2015-05-09 01:54:14 +0200

    DOC - MoveService & StreamService
    Documentation de l'API REST, dans la javadoc.

--- GoPiGo/src/server/rest/MoveService.java
+++ GoPiGo/src/server/rest/MoveService.java
@@ -9,20 +9,50 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import server.Globals;
- 
+
+/**
+ *		********	MOVE SERVICE	********
+ *
+ * 	Service REST permettant de contrôler le robot.
+ * 	Les fonctions disponibles permettent de : 
+ * 		- lui donner un mouvement {@link #move}
+ * 		- le stopper {@link #stop}
+ * 		- modifier sa vitesse {@link #setSpeed}
+ * 
+ *	Chaque méthode est documentée avec un exemple de cas d'utilisation.
+ */
 @Path("/gopigo")
 public class MoveService {
 
-	// exemple d'utilisation : http://localhost:8080/gopigo/move/right
+	/**
+	 * 	Fait éxécuter au GoPiGo le mouvement indiqué.
+	 * 	Le mouvement continuera jusqu'à un appel à la fonction {@link #stop}.
+	 * 	Les valeurs possibles de direction sont : 
+	 * 		- FORWARD
+	 * 		- BACKWARD
+	 * 		- RIGHT
+	 * 		- LEFT
+	 * 		- STOP (préférer {@link #stop} )
+	 * 
+	 * 	Toute autre direction donnée ne produira aucun effet.
+	 * 
+	 * 	Attention : le robot commencera le mouvmeent instantanément. Vérifier son emplacement au préalable !
+	 * 
+	 * 		usage:	http://adressePi:8080/gopigo/move/right
+	 * 
+	 * @param direction	- [String] direction 
+	 * @return	"Moving $direction..." | "Invalid Direction"
+	 * @throws IOException
+	 */
     @GET
     @Path("/move/{direction}")
     @Produces(MediaType.TEXT_PLAIN)
     public String move( @PathParam("direction") String direction ) throws IOException {
 
-    	direction = direction.toUpperCase() ;
+    	direction = direction.toUpperCase() ;	// ignoreCase
 		try
 		{
-			if(direction.equals("RIGHT"))
+			if(direction.equals("RIGHT"))		// Java 6 compliance : pas de switch...
 			{
 				Globals.gopigo.turnRight();
 			}
@@ -49,6 +79,15 @@ public class MoveService {
     	return "Moving "+direction.toLowerCase()+"...\n" ;
     }
     
+    /**
+     * 	Stoppe le robot si celui-ci est en mouvement.
+     * 	Réflexe à develloper en environnement hostile...
+     * 
+	 * 		usage:	http://adressePi:8080/gopigo/stop
+     * 
+     * @return "Stopped"
+     * @throws IOException
+     */
     @GET
     @Path("/stop")
     @Produces(MediaType.TEXT_PLAIN)
@@ -58,30 +97,58 @@ public class MoveService {
     	return "Stopped !\n" ;
     }
     
+    /**
+     * 	Fixe la vitesse du GoPiGo à une valeur donnée.
+     * 	Si la valeur n'est pas un entier, ne fait rien et le signale en retour.
+     * 	Valeur minimale : 
+     * 	Valeur maximale :
+     * 
+	 * 		usage:	http://adressePi:8080/gopigo/speed/set/85
+     * 
+     * @param speed - [String] Valeur entière et positive
+     * @return	"Set speed to "+speed+"
+     * @throws IOException
+     */
     @GET
     @Path("/speed/set/{speed}")
     @Produces(MediaType.TEXT_PLAIN)
     public String setSpeed(@PathParam("speed") String speed) throws IOException {
 
-    	
+    	//TODO
     	return "Set speed to "+speed+".\n" ;
     }
     
+    /**
+     * 	Augmente la vitesse du GoPiGo.
+     * 
+	 * 		usage:	http://adressePi:8080/gopigo/speed/increase
+     * 
+     * @return	"Speed ++"
+     * @throws IOException
+     */
     @GET
     @Path("/speed/increase")
     @Produces(MediaType.TEXT_PLAIN)
     public String speedIncrease() throws IOException {
 
-    	
+    	//TODO
     	return "Speed ++\n" ;
     }
     
+    /**
+     * 	Diminue la vitesse du GiPiGo.
+     * 
+	 * 		usage:	http://adressePi:8080/gopigo/speed/decrease
+     * 
+     * @return	"Speed --"
+     * @throws IOException
+     */
     @GET
     @Path("/speed/decrease")
     @Produces(MediaType.TEXT_PLAIN)
     public String speedDecrease() throws IOException {
 
-    	
+    	//TODO
     	return "Speed --\n" ;
     }
     
--- GoPiGo/src/server/rest/StreamService.java
+++ GoPiGo/src/server/rest/StreamService.java
@@ -9,16 +9,40 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import server.Globals;
 
+/**
+ *		********	STREAM SERVICE	********
+ *
+ * 	Service REST permettant d'utiliser le module caméra du GoPiGo.
+ * 	Compte tenu de la puissance limitée du pi, et de la consommation électrique
+ * 	relativement élevée d'un tel dispositif, le choix a été fait d'utiliser
+ * 	des photos prises à intervalles réguliers au lieu d'un flux vidéo.
+ * 	Ainsi, la bande passante est économisée, et le pi n'a pas à faire d'encodage vidéo (exercice ou il est mauvais).
+ * 
+ * 	Les fonctions disponibles permettent de : 
+ * 		- démarrer le streaming {@link #start}
+ * 		- le stopper {@link #stop}
+ * 		- récupérer la frame actuelle {@link #getStream}
+ * 
+ *	Chaque méthode est documentée avec un exemple de cas d'utilisation.
+ */
 @Path("/video")
 public class StreamService {
 
+	/**
+	 * 	Récupère la dernière image enregistrée.
+	 * 	Attention : Impact important sur la bande passante.
+	 * 
+	 * 		usage:	http://adressePi:8080/video/get
+	 * 
+	 * @return	[BitMap File] - la dernière frame enregistrée
+	 * @throws IOException
+	 */
     @GET
     @Path("/get")
     @Produces("image/bmp")
@@ -36,6 +60,19 @@ public class StreamService {
     	    return Response.ok(new ByteArrayInputStream(imageData)).build();
     }
     
+    /**
+     * 	Démarre le processus d'acquisition vidéo.
+     * 	Une fois ce processus activé, la frame rendue par {@link #getStream()} changera à intervalle régulier.
+     * 	Cet intervalle est modifiable dans {@link SettingsService.setFPS}.
+     * 
+     * 	Attention : Impact important sur l'utilisation du CPU
+     * 	Bonne pratique : Stopper le streaming lorsqu'il n'est plus utilisé. {@link #stop()}
+     * 
+	 * 		usage:	http://adressePi:8080/video/start
+     * 
+     * @return	[String] -	"Streaming started !"
+     * @throws IOException
+     */
     @GET
     @Path("/start")
     @Produces(MediaType.TEXT_PLAIN)
@@ -46,10 +83,19 @@ public class StreamService {
     	return "Streaming started !\n" ;
     }
     
+    /**
+     * 	Stoppe le processus d'acquisition vidéo.
+     * 	La dernière image enregistrée reste accessible.
+     * 
+	 * 		usage:	http://adressePi:8080/video/stop
+	 * 
+     * @return
+     * @throws IOException
+     */
     @GET
     @Path("/stop")
     @Produces(MediaType.TEXT_PLAIN)
-    public String stop(@PathParam("speed") String speed) throws IOException {
+    public String stop() throws IOException {
 
     	Globals.gopigo.stopStreaming();
     	System.out.println("[StreamService] : stopped.");
